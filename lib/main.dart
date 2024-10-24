5bde7d3f47816eb06b199569af806f4a2268700e
mohammed hussein <muhammedhussein739@gmail.com>
2024-10-24 17:36:09 +0300

    integrate with firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
@@ -8,6 +9,7 @@ import 'package:todo_app/utils/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   await CacheData.cacheInitialization();
   runApp(
     ChangeNotifierProvider(
