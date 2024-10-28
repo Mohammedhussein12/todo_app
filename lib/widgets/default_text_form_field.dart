fa289a31c4cc66bd93872b5351bb696159d03f26
mohammed hussein <muhammedhussein739@gmail.com>
2024-10-28 20:54:12 +0300

    add some changes in text form field.

--- lib/widgets/default_text_form_field.dart
+++ lib/widgets/default_text_form_field.dart
@@ -5,27 +5,30 @@ import '../tabs/settings/settings_provider.dart';
 import '../utils/app_theme.dart';
 
 class DefaultTextFormField extends StatelessWidget {
-  const DefaultTextFormField({
-    super.key,
-    required this.hintText,
-    required this.controller,
-    this.validator,
-  });
+  const DefaultTextFormField({super.key,
+      required this.hintText,
+      required this.controller,
+      this.validator,
+      this.keyboardType = TextInputType.text});
 
   final String hintText;
   final TextEditingController controller;
   final String? Function(String?)? validator;
+  final TextInputType? keyboardType;
 
   @override
   Widget build(BuildContext context) {
     var settingsProvider = Provider.of<SettingsProvider>(context);
     return TextFormField(
+      keyboardType: keyboardType,
+      cursorColor: settingsProvider.isDark ? AppTheme.white : AppTheme.black,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      style: Theme.of(context).textTheme.titleMedium,
       validator: validator,
       controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
-        hintStyle: TextStyle(
-            color: settingsProvider.isDark ? AppTheme.white : AppTheme.black),
+        hintStyle: Theme.of(context).textTheme.titleMedium,
       ),
     );
   }
